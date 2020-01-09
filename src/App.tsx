3bcd28d426c2c9bcbc5194686dbcf40ed1124ad7
luckygro <lukas@luckyg.de>
2020-01-09 23:52:43 +0100

    first day of work

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faEnvelope, faMobile } from '@fortawesome/free-solid-svg-icons';
+import Contactform from './components/contactform';
+import Mailto from 'react-protected-mailto';
+ 
 const App: React.FC = () => {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* eyecatcher section */}
+      <section id="eyecatcher">
+
+        <header className="container">
+          <nav>
+            <ul>
+              <li><a href="/">home</a></li>
+              <li><a href="/#contact">contact</a></li>
+              <li><a href="/legal">legal</a></li>
+            </ul>
+          </nav>
+          <div className="logo">
+            <span>luckyg.design</span>
+            <img src="/images/logo.png" alt="Luckyg-Design Logo Icon" />
+          </div>
+        </header>
+
+        <div className="eyecatcher-content">
+          <h2>hey, i'm</h2>
+          <h1>LUKAS</h1>
+          <a className="button-cta" href="#contact">get in touch</a>
+        </div>
+
+        <div className="eyecatcher-scroll">
+          <FontAwesomeIcon icon={faChevronDown} />
+        </div>
+
+      </section>
+
+      {/* contact section */}
+      <section id="contact">
+
+        <div className="contact-intro">
+          <h2>let's get in touch</h2>
+          <p>I'd love to get in contact with you - if you like feel free to call or shoot a message!</p>
+          <p>You can also use the contact form below and I will contact you back...</p>
+
+          <div className="contact-email">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <Mailto email="lukas@luckyg.de" />
+          </div>
+
+          <div className="contact-phone">
+            <FontAwesomeIcon icon={faMobile} />
+            <Mailto tel="+49 176 5763 7942" />
+          </div>
+
+        </div>
+
+        <Contactform />
+      </section>
+
+      <footer>
+
+        <div className="container">
+
+          <div className="footer-company">
+            <span className="company-name">LUCKYG.DESIGN</span>
+            <span className="company-owner">Inhaber: Lukas Großmann</span>
+            <span className="company-addres">Schanzstraße 35, 75446 Wiernsheim, GERMANY</span>
+          </div>
+
+          <div className="footer-legal">
+            <a href="/datenschutzerklärung">Datenschutzerklärung</a>
+            <a href="/impressum">Impressum</a>
+            <span className="legal-credits">Layout&Design: Lukas Großmann</span>
+          </div>
+
+        </div>
+
+      </footer>
+
     </div>
+
   );
 }
 
---
+++ src/components/contactform.tsx
@@ -0,0 +1,155 @@
+import React from 'react';
+
+type IProps = {
+
+}
+
+type IState = {
+  status: string,
+  statusMessage: string,
+  data: {
+    name: string,
+    email: string,
+    message: string,
+    confirm: boolean
+  }
+}
+
+class Contactform extends React.Component<IProps, IState> {
+  state = {
+    status: 'default',
+    statusMessage: '',
+    data: {
+      name: '',
+      email: '',
+      message: '',
+      confirm: false,
+    },
+  }
+
+  submit = e => {
+    e.preventDefault()
+
+    if (!this.state.data.confirm) {
+      this.setState({statusMessage: 'bitte bestätigen Sie die Datenschutzerklärung', status: 'form-pending'});
+      return;
+    }
+
+    this.setState({statusMessage: 'Email wird gesendet, bitte warten', status: 'form-sending'});
+
+    let {
+      data: { name, email, message },
+    } = this.state
+
+    fetch('https://us-central1-handle-fcb54.cloudfunctions.net/addMessage', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        message: message,
+        project: 'luckyg',
+      }),
+    }).then(res => {
+      if (!res.ok) {
+        throw Error(res.toString());
+      }
+      return res;
+    }).then(res => {
+      this.setState({ statusMessage: 'Email wurde erfolgreich gesendet', status:'form-success'});
+    }).catch(err => {
+      this.setState({ statusMessage: 'Email konnte nicht versendet werden', status:'form-failed'});
+    })
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState(state => ({
+      data: Object.assign({}, state.data, { [name]: value }),
+    }))
+  }
+
+  toggleDatenschutz = ({target: {checked}}) => {
+    this.setState(state => ({
+      data: Object.assign({}, state.data, { confirm: checked }),
+    }))
+  }
+
+  render() {
+    let { data } = this.state
+
+    return (
+      <form
+        id="ContactForm"
+        action="https://us-central1-handle-fcb54.cloudfunctions.net/addMessage"
+        onSubmit={this.submit}
+      >
+        <div className="small">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            required
+            placeholder="Vorname Nachname"
+            value={data.name}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div className="small">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="ich@example.de"
+            value={data.email}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div className="big">
+          <label>Nachricht</label>
+          <textarea
+            name="message"
+            required
+            placeholder="Hallo..."
+            rows={7}
+            value={data.message}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div className="small dsgvo-confirm">
+          <input
+            type="checkbox"
+            name="confirm"
+            required
+            placeholder="i"
+            onChange={this.toggleDatenschutz}
+          />
+          <label>
+            Ich habe die <a href="/datenschutz">Datenschutzerklärung</a> zur Kenntnis genommen. Ich stimme zu, dass meine Angaben und Daten zur Beantwortung meiner Anfrage elektronisch erhoben und gespeichert werden.
+            Hinweis: Sie können Ihre Einwilligung jederzeit für die Zukunft per E-Mail an <a href='lukas@luckyg.de'>lukas@luckyg.de</a> widerrufen.
+          </label>
+
+        </div>
+
+        <div className="big contact-buttons">
+          <button className="button" type="submit">Absenden</button>
+
+        </div>
+
+
+        <div className={`big formstatus ${this.state.status}`}>
+          <span>{this.state.statusMessage}</span>
+        </div>
+
+      </form>
+    )
+  }
+}
+
+export default Contactform
